dbc6896c5c161adb4f2708dedd17e2188d0f448e
tatirusmiati95 <32425968+tatirusmiati95@users.noreply.github.com>
2018-03-23 09:39:37 +0800

    Memanggil Method

--- src/praktek02/BuahBuahanAksi.java
+++ src/praktek02/BuahBuahanAksi.java
@@ -10,40 +10,39 @@ package praktek02;
  * @author ASUS
  */
 public class BuahBuahanAksi {
+
     public static void main(String[] args) {
-        
-        
-BuahBuahan Rambutan = new BuahBuahan();
 
+        BuahBuahan Rambutan = new BuahBuahan();
 
-         Rambutan.bentuknya="Bundar";
-        Rambutan.rasanya="Manis";
-        Rambutan.warnanya="Merah Daging Putih";
-        Rambutan.ukurannya="Kecil";
-        Rambutan.kandungannya="Vitamin A,B&C";
-        Rambutan.caramenanamnya="Tanam Bijinya";
-        
+        Rambutan.bentuknya = "Bundar";
+        Rambutan.rasanya = "Manis";
+        Rambutan.warnanya = "Merah Daging Putih";
+        Rambutan.ukurannya = "Kecil";
+        Rambutan.kandungannya = "Vitamin A,B&C";
+        Rambutan.caramenanamnya = "Tanam Bijinya";
+        Rambutan.cetakInfo();
 
-BuahBuahan Anggur = new BuahBuahan();
+        BuahBuahan Anggur = new BuahBuahan();
 
-         Anggur.bentuknya="Bundar";
-        Anggur.rasanya="Manis";
-        Anggur.warnanya="Ungu Daging Putih";
-        Anggur.ukurannya="Sangat Kecil";
-        Anggur.kandungannya="Vitamin A";
-        Anggur.caramenanamnya="Tanam Bijinya";
-        
+        Anggur.bentuknya = "Bundar";
+        Anggur.rasanya = "Manis";
+        Anggur.warnanya = "Ungu Daging Putih";
+        Anggur.ukurannya = "Sangat Kecil";
+        Anggur.kandungannya = "Vitamin A";
+        Anggur.caramenanamnya = "Tanam Bijinya";
+        Anggur.cetakInfo();
 
-BuahBuahan Kelapa = new BuahBuahan();
+        BuahBuahan Kelapa = new BuahBuahan();
 
-         Kelapa.bentuknya="Bundar";
-        Kelapa.rasanya="Manis";
-        Kelapa.warnanya="Hijau Daging Putih";
-        Kelapa.ukurannya="Besar";
-        Kelapa.kandungannya="Vitamin A,B&C";
-        Kelapa.caramenanamnya="Tanam Tunasnya";
-        
-        
+        Kelapa.bentuknya = "Bundar";
+        Kelapa.rasanya = "Manis";
+        Kelapa.warnanya = "Hijau Daging Putih";
+        Kelapa.ukurannya = "Besar";
+        Kelapa.kandungannya = "Vitamin A,B&C";
+        Kelapa.caramenanamnya = "Tanam Tunasnya";
+
+        Kelapa.cetakInfo();
     }
-    
+
 }
